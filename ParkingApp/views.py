e341dc6b81835df25357b732ba9eb4334cc1ed7c
VladCalo <80626362+VladCalo@users.noreply.github.com>
2023-11-12 14:20:23 +0200

    added views, serializers and urls for  Basic CRUD endpoints and also with pk

--- ParkingApp/views.py
+++ ParkingApp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import ParkOwner, Users, Credentials, Park, ParkDetails, Floor, ParkingSlot, ParkingSlotRules, Booking
+from .serializers import ParkOwnerSerializer, UsersSerializer, CredentialsSerializer, ParkSerializer, ParkDetailsSerializer, FloorSerializer, ParkingSlotSerializer, ParkingSlotRulesSerializer, BookingSerializer
+
+class ParkOwnerViewSet(viewsets.ModelViewSet):
+    queryset = ParkOwner.objects.all()
+    serializer_class = ParkOwnerSerializer
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+    
+class CredentialsViewSet(viewsets.ModelViewSet):
+    queryset = Credentials.objects.all()
+    serializer_class = CredentialsSerializer
+
+class ParkViewSet(viewsets.ModelViewSet):
+    queryset = Park.objects.all()
+    serializer_class = ParkSerializer
+
+class ParkDetailsViewSet(viewsets.ModelViewSet):
+    queryset = ParkDetails.objects.all()
+    serializer_class = ParkDetailsSerializer
+
+class FloorViewSet(viewsets.ModelViewSet):
+    queryset = Floor.objects.all()
+    serializer_class = FloorSerializer
+
+class ParkingSlotViewSet(viewsets.ModelViewSet):
+    queryset = ParkingSlot.objects.all()
+    serializer_class = ParkingSlotSerializer
+
+class ParkingSlotRulesViewSet(viewsets.ModelViewSet):
+    queryset = ParkingSlotRules.objects.all()
+    serializer_class = ParkingSlotRulesSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
